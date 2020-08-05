ebc45c40b6175751eaa8834d09651a17bef4b3f3
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-05 22:46:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,6 +13,7 @@ class Artist
   
   def songs 
     Song.all.select {|song| song.artist == self}
+    @@song_count += 1
   end
   
   def add_song(song)
@@ -24,7 +25,6 @@ class Artist
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
-    binding.pry
   end 
   
   def self.song_count
