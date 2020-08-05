028376ecf820f68d836f863bc5437ed1366ed493
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-05 22:44:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,11 +21,9 @@ class Artist
     @@song_count += 1
   end 
   
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song 
-    song.artist = self
-    @@song_count += 1
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
   end 
   
   def self.song_count
